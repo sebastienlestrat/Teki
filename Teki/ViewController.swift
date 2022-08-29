ecdc8dbb15c13cb38a6dda48a1a31003f0d0594b
Sébastien Le Strat <sebastien.le-strate@hotmail.fr>
2022-08-29 23:00:49 +0200

    Initial Commit

---
+++ Teki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teki
+//
+//  Created by Sébastien LE STRAT on 29/08/2022.
+//  Copyright © 2022 com.openclassrooms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
